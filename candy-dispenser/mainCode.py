3feef28a0d5006d87f16f107e1fc0b8762d33cf6
H. Kamran <hkamran80@users.noreply.github.com>
2017-03-02 15:30:39 -0800

    Update mainCode.py

--- candy-dispenser/mainCode.py
+++ candy-dispenser/mainCode.py
@@ -1,16 +1,26 @@
 import RPi.GPIO as GPIO
+import datetime as dt
 
 #setup GPIO using Board numbering
 GPIO.setmode(GPIO.BOARD)
 
+Motor1A = 2
+Motor1B = 3
+Motor1C = 4
+Motor1D = 18
+
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
   if(GPIO.input(23) ==1):
     print('Dispensing Jolly Ranchers.')
+    GPIO.output(Motor1A,GPIO.HIGH)
+    time.sleep(5)
+  if(GPIO.input(24) ==1):
+    print('Dispensing caramel.')
     
 
 print('Activating.')
